3548fc7a71dbf1dcb0b811810e08d63ce89ab6f1
email <henrygiovanniangel@gmail.com>
2024-06-14 11:10:26 -0500

    first commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,51 @@
+package org.example
+
+fun main(){
+
+}
+/*fun main() {
+    val trafficLightColor = "Amber"
+
+    val message = when(trafficLightColor) {
+        "Red" -> "Stop"
+        "Yellow", "Amber" -> "Proceed with caution."
+        "Green" -> "Go"
+        else -> "Invalid traffic-light color"
+    }
+    println(message)*/
+
+
+/*fun main() {
+    val x: Any = 20
+
+    when (x) {
+        2, 3, 5, 7 -> println("x is a prime number between 1 and 10.")
+        in 1..10 -> println("x is a number between 1 and 10, but not a prime number.")
+        is Int -> println("x is an integer number, but not between 1 and 10.")
+        else -> println("x isn't an integer number.")
+    }
+}*/
+/*fun main() {
+    val trafficLightColor = "Black"
+
+    if (trafficLightColor == "Red") {
+        println("Stop")
+    } else if (trafficLightColor == "Yellow") {
+        println("Slow")
+    } else if (trafficLightColor == "Green") {
+        println("Go")
+    } else {
+        println("Invalid traffic-light color")
+    }
+
+}*/
+/*fun main() {
+    val trafficLightColor = "Amber"
+
+    when (trafficLightColor) {
+        "Red" -> println("Stop")
+        "Yellow", "Amber" -> println("Slow")
+        "Green" -> println("Go")
+        else -> println("Invalid traffic-light color")
+    }
+}*/
